b972db88edd1cd4f892d7304ad1eab750bf1db06
harisajjad <106540176+harisajjad@users.noreply.github.com>
2024-05-06 06:42:55 +1000

    Add files via upload

---
+++ Controllers/TodosController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using TodoApi.Services;
+using TodoApi.ViewModel;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private readonly ITodoService _service;
+
+        public TodosController(ITodoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
+        {
+            return Ok(await _service.GetAllTodos());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> GetTodo(Guid id)
+        {
+            var todo = await _service.GetTodoById(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+
+        [HttpPost]
+        //[Route("createTodos")]
+        public async Task<ActionResult<Todo>> CreateTodo(ToDoViewModel todoVM)
+        {
+
+            Todo todo = new Todo();
+
+            todo.Id = Guid.NewGuid();
+            todo.Task = todoVM.Task;
+
+            var createdTodo = await _service.CreateTodo(todo);
+            return CreatedAtAction(nameof(GetTodo), new { id = createdTodo.Id }, createdTodo);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodo(Guid id, ToDoViewModel todoVM)
+        {
+            Todo todo = new Todo();
+            todo.Id = id;
+            todo.Task = todoVM.Task;
+
+            await _service.UpdateTodo(id, todo);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(Guid id)
+        {
+            await _service.DeleteTodo(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TodoApi.AppDbContext;
+using TodoApi.Repository;
+using TodoApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+builder.Services.AddScoped<ITodoService, TodoService>();
+
+var app = builder.Build();
+app.UseCors("CorsPolicy");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
